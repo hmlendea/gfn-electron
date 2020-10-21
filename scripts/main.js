12c44d17109b5a96825d9a3540d44c3123acb92f
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-10-22 00:47:57 +0300

    Removed te ChromeOS UA

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 const {app, globalShortcut, BrowserWindow} = require('electron')
 const path = require('path')
-const userAgent = 'Mozilla/5.0 (X11; CrOS x86_64 13099.85.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.110 Safari/537.36';
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
@@ -35,7 +34,6 @@ app.whenReady().then(() => {
 app.on('browser-window-created', function(e, window) {
   window.setBackgroundColor('#1A1D1F');
   window.setMenu(null);
-  window.webContents.setUserAgent(userAgent);
 
   if (window.id != 1) {
     var mainWindow = BrowserWindow.fromId(1);
