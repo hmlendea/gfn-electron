9975917cf5eb6b10041e068d5cb5561b52636ef4
raluvy95 <ralucadoge069@gmail.com>
2021-05-19 11:20:11 +0300

    Added Discord RPC

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
-const {app, globalShortcut, BrowserWindow} = require('electron');
+const { app, globalShortcut, BrowserWindow } = require('electron');
+const DiscordRPC = require("discord-rpc");
 const path = require('path');
+const configure = require("./../config.json")
 
 var isFullScreen = false;
+const rpc = new DiscordRPC.Client({ transport: "ipc" });
+const starttimestamp = new Date();
+const clientId = configure.clientID;
+
+let RPC = {
+  details: `Playing`,
+  startTimestamp: starttimestamp,
+  largeImageKey: "nvidia"
+}
+
+rpc.on("ready", () => {
+  rpc.setActivity(RPC);
+});
+rpc.login({ clientId }).catch(console.error);
 
 app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecoder');
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
@@ -14,11 +30,17 @@ function createWindow () {
   });
   mainWindow.webContents.userAgent = "Mozilla/5.0 (X11; CrOS x86_64 13816.55.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.86 Safari/537.36";
   mainWindow.loadURL('https://play.geforcenow.com');
+  mainWindow.on("page-title-updated", (event, title) => {
+    console.log(title)
+    if (title == "GeForce NOW") RPC.details = "Main Menu";
+    else RPC.details = `Playing ${title.slice(0, 5)}`;
+    rpc.setActivity(RPC);
+  })
 }
 
 app.whenReady().then(() => {
   createWindow();
-  
+
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -37,17 +59,17 @@ app.whenReady().then(() => {
   });
 });
 
-app.on('browser-window-created', function(e, window) {
+app.on('browser-window-created', function (e, window) {
   window.setBackgroundColor('#1A1D1F');
   window.setMenu(null);
 
-  window.on('leave-full-screen', function(e, win) {
+  window.on('leave-full-screen', function (e, win) {
     if (isFullScreen) {
       BrowserWindow.getAllWindows()[0].setFullScreen(true);
     }
   });
 
-  window.on('page-title-updated', function(e, title) {
+  window.on('page-title-updated', function (e, title) {
     if (title.includes('on GeForce NOW')) {
       window.setFullScreen(true);
       isFullScreen = true;
