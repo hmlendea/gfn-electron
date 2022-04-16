1145f03f612ea879f4a92a2b6d17430bce2cfa22
dubfib <81378985+dubfib@users.noreply.github.com>
2022-04-16 14:59:52 -0700

    Add files via upload

--- scripts/rpc.js
+++ scripts/rpc.js
@@ -1,8 +1,22 @@
 const client = require('discord-rich-presence')('963128360219869194');
 
+const pkg = require('../package.json');
+const args = pkg.scripts.start;
+
 function DiscordRPC(title) {
+    if (args.includes('--disable-rpc')) return;
+
+    let d;
+
+    if (title.includes('on GeForce NOW')) {
+        d = title;
+    } else {
+        d = "Home on GeForce NOW";
+    };
+
     client.updatePresence({
-        details: title.replace("Geforce", "GeForce"),
+        details: d,
+        state: `Not affiliated with NVIDIA`,
         startTimestamp: Date.now(),
         largeImageKey: 'icon',
         instance: true,
