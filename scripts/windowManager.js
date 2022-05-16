cc95166e4f5ce211211285b119f17fa61fa374a0
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-05-16 08:35:14 +0300

    Prevent exiting fullscreen while game streaming

--- scripts/windowManager.js
+++ scripts/windowManager.js
@@ -1,14 +1,26 @@
 const { app, globalShortcut, BrowserWindow } = require('electron');
 
 var isFullScreen = false;
+var isGameStreamingScreen = false;
 
 function toggleFullscreen(state) {
     var actualState = BrowserWindow.getAllWindows()[0].isFullScreen();
     if (isFullScreen != state || actualState != state) {
-        BrowserWindow.getAllWindows()[0].setFullScreen(state);
-        isFullScreen = state;
-        console.log("Fullscreen state changed to: " + state);
+        if (state || !isGameStreamingScreen) {
+            BrowserWindow.getAllWindows()[0].setFullScreen(state);
+            isFullScreen = state;
+            console.log("Fullscreen state changed to: " + state);
+        }
     }
+}
+
+function toggleGameStreamingMode(state) {
+    if (isGameStreamingScreen != state) {
+        isGameStreamingScreen = state;
+        console.log("Game streaming mode state changed to: " + state);
+    }
+
+    toggleFullscreen(isGameStreamingScreen);
 }
 
 function switchFullscreenState() {
@@ -20,13 +32,18 @@ function switchFullscreenState() {
 }
 
 app.on('browser-window-created', async function (event, window) {
-    window.on("enter-screen", async function(event, window) {
+    window.on("enter-full-screen", async function(event, window) {
         event.preventDefault();
         toggleFullscreen(true);
     });
+    window.on("leave-full-screen", async function(event, window) {
+        event.preventDefault();
+        if (isGameStreamingScreen) {
+            toggleFullscreen(true);
+        }
+    });
     window.on('page-title-updated', async function (event, title) {
-        var isGameStreamingScreen = title.includes('on GeForce NOW');
-        toggleFullscreen(isGameStreamingScreen);
+        toggleGameStreamingMode(title.includes('on GeForce NOW'));
     });
 });
 
