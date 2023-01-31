1bd376541b2d3dfbd2d34a4be3458410f4bdcad5
Chris Boucrot <chris.boucrot@gmail.com>
2023-01-31 17:43:43 +0100

    Improve code style

--- scripts/main.js
+++ scripts/main.js
@@ -3,152 +3,177 @@ const path = require('path');
 const { DiscordRPC } = require('./rpc.js');
 const { switchFullscreenState } = require('./windowManager.js');
 
-var homePage = 'https://play.geforcenow.com';
+//> Definitions
+const HOME_PAGE_URL = 'https://play.geforcenow.com';
+const DIRECT_START_URL = 'https://play.geforcenow.com/mall/#/streamer?launchSource=GeForceNOW&cmsId=';
 
-  var userAgent =
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.101 Safari/537.36'; // Linux
+const DEFAULT_UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.101 Safari/537.36';
+const WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36';
+const CHROME_UA = 'Mozilla/5.0 (X11; CrOS x86_64 14909.100.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.83 Safari/537.36';
 
-  if (process.argv.includes('--spoof-chromeos')) {
-    userAgent =
-      'Mozilla/5.0 (X11; CrOS x86_64 14909.100.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.83 Safari/537.36'; // ChromeOS
-    app.commandLine.appendSwitch('disable-features', 'UserAgentClientHint');
-  }
+const OPT_SPOOF_WIN = '--spoof-windows';
+const OPT_SPOOF_CHROMEOS = '--spoof-chromeos';
+const OPT_DIRECT_START = '--direct-start';
 
-  if (process.argv.includes('--spoof-windows')) {
-    userAgent =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'; // Windows
-    app.commandLine.appendSwitch('disable-features', 'UserAgentClientHint');
-  }
+let userAgent = DEFAULT_UA;
+let game;
 
-  console.log('Using user agent: ' + userAgent);
-  console.log('Process arguments: ' + process.argv);
+const spoofWindows = process.argv.includes(OPT_SPOOF_WIN);
+const spoofChromeOS = process.argv.includes(OPT_SPOOF_CHROMEOS);
+const directStart = process.argv.includes(OPT_DIRECT_START);
+//< Definitions
 
-  app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecoder,WaylandWindowDecorations');
-  
-  app.commandLine.appendSwitch(
-    'disable-features',
-    'UseChromeOSDirectVideoDecoder'
-  );
-  app.commandLine.appendSwitch('enable-accelerated-mjpeg-decode');
-  app.commandLine.appendSwitch('enable-accelerated-video');
-  app.commandLine.appendSwitch('ignore-gpu-blacklist');
-  app.commandLine.appendSwitch('enable-native-gpu-memory-buffers');
-  app.commandLine.appendSwitch('enable-gpu-rasterization');
+//> Retrieving cli options
+if (spoofWindows) {
+  userAgent = WIN_UA;
+}
 
-  async function createWindow() {
-    const mainWindow = new BrowserWindow({
-      fullscreenable: true,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
-        contextIsolation: false,
-        userAgent: userAgent,
-      },
-    });
+if (spoofChromeOS) {
+  userAgent = CHROME_UA;
+}
 
-    if (process.argv.includes('--direct-start')) {
-      mainWindow.loadURL('https://play.geforcenow.com/mall/#/streamer?launchSource=GeForceNOW&cmsId=' + process.argv[process.argv.indexOf('--direct-start') + 1]);
-    } else {
-      mainWindow.loadURL(homePage);
-    }
+if (directStart) {
+  game = process.argv[process.argv.indexOf(OPT_DIRECT_START) + 1];;
+}
 
-    /*
-    uncomment this to debug any errors with loading GFN landing page
+console.log('Using user agent: ' + userAgent);
+console.log('Process arguments: ' + process.argv);
+//< Retrieving cli options
 
-    mainWindow.webContents.on("will-navigate", (event, url) => {
-      console.log("will-navigate", url);
-      event.preventDefault();
-    });
-    */
-  }
+//> Config
+if (spoofWindows || spoofChromeOS) {
+  app.commandLine.appendSwitch('disable-features', 'UserAgentClientHint');
+}
 
-  app.whenReady().then(async () => {
-    createWindow();
+app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecoder,WaylandWindowDecorations');
 
-    DiscordRPC('GeForce NOW');
+app.commandLine.appendSwitch(
+  'disable-features',
+  'UseChromeOSDirectVideoDecoder'
+);
+app.commandLine.appendSwitch('enable-accelerated-mjpeg-decode');
+app.commandLine.appendSwitch('enable-accelerated-video');
+app.commandLine.appendSwitch('ignore-gpu-blacklist');
+app.commandLine.appendSwitch('enable-native-gpu-memory-buffers');
+app.commandLine.appendSwitch('enable-gpu-rasterization');
+//< Config
 
-    app.on('activate', async function() {
-      if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-      }
-    });
-
-    globalShortcut.register('Super+F', async () => {
-      switchFullscreenState();
-    });
-
-    globalShortcut.register('F11', async () => {
-      switchFullscreenState();
-    });
-
-    globalShortcut.register('Alt+F4', async () => {
-      app.quit();
-    });
-
-    globalShortcut.register('Alt+Home', async () => {
-      BrowserWindow.getAllWindows()[0].loadURL(homePage);
-    });
-
-    globalShortcut.register('F4', async () => {
-      app.quit();
-    });
-
-    globalShortcut.register('Control+Shift+I', () => {
-      BrowserWindow.getAllWindows()[0].webContents.toggleDevTools();
-    });
-
-    globalShortcut.register('Esc', async () => {
-      var window = BrowserWindow.getAllWindows()[0];
-
-      window.webContents.sendInputEvent({
-        type: 'keyDown',
-        keyCode: 'Esc'
-      });
-      window.webContents.sendInputEvent({
-        type: 'char',
-        keyCode: 'Esc'
-      });
-      window.webContents.sendInputEvent({
-        type: 'keyUp',
-        keyCode: 'Esc'
-      });
-
-      window.webContents.sendInputEvent({
-        type: 'keyDown',
-        keyCode: 'Esc'
-      });
-      window.webContents.sendInputEvent({
-        type: 'char',
-        keyCode: 'Esc'
-      });
-      window.webContents.sendInputEvent({
-        type: 'keyUp',
-        keyCode: 'Esc'
-      });
-    });
+//> Start main process
+async function createWindow() {
+  const mainWindow = new BrowserWindow({
+    fullscreenable: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false,
+      userAgent: userAgent,
+    },
   });
 
-  app.on('browser-window-created', async function(e, window) {
-    window.setBackgroundColor('#1A1D1F');
-    window.setMenu(null);
+  if (directStart) {
+    mainWindow.loadURL(DIRECT_START_URL + game);
+  } else {
+    mainWindow.loadURL(HOME_PAGE_URL);
+  }
 
-    window.webContents.setUserAgent(userAgent);
+  /*
+  uncomment this to debug any errors with loading GFN landing page
 
-    window.webContents.on('new-window', (event, url) => {
-      event.preventDefault();
-      BrowserWindow.getAllWindows()[0].loadURL(url);
-    });
-
-    window.on('page-title-updated', async function(e, title) {
-      DiscordRPC(title);
-    });
+  mainWindow.webContents.on("will-navigate", (event, url) => {
+    console.log("will-navigate", url);
+    event.preventDefault();
   });
+  */
+}
 
-  app.on('will-quit', async () => {
-    globalShortcut.unregisterAll();
-  });
+app.whenReady().then(async () => {
+  createWindow();
 
-  app.on('window-all-closed', async function() {
-    if (process.platform !== 'darwin') {
-      app.quit();
+  DiscordRPC('GeForce NOW');
+
+  app.on('activate', async function() {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
     }
   });
+
+  globalShortcut.register('Super+F', async () => {
+    switchFullscreenState();
+  });
+
+  globalShortcut.register('F11', async () => {
+    switchFullscreenState();
+  });
+
+  globalShortcut.register('Alt+F4', async () => {
+    app.quit();
+  });
+
+  globalShortcut.register('Alt+Home', async () => {
+    BrowserWindow.getAllWindows()[0].loadURL(homePage);
+  });
+
+  globalShortcut.register('F4', async () => {
+    app.quit();
+  });
+
+  globalShortcut.register('Control+Shift+I', () => {
+    BrowserWindow.getAllWindows()[0].webContents.toggleDevTools();
+  });
+
+  globalShortcut.register('Esc', async () => {
+    var window = BrowserWindow.getAllWindows()[0];
+
+    window.webContents.sendInputEvent({
+      type: 'keyDown',
+      keyCode: 'Esc'
+    });
+    window.webContents.sendInputEvent({
+      type: 'char',
+      keyCode: 'Esc'
+    });
+    window.webContents.sendInputEvent({
+      type: 'keyUp',
+      keyCode: 'Esc'
+    });
+
+    window.webContents.sendInputEvent({
+      type: 'keyDown',
+      keyCode: 'Esc'
+    });
+    window.webContents.sendInputEvent({
+      type: 'char',
+      keyCode: 'Esc'
+    });
+    window.webContents.sendInputEvent({
+      type: 'keyUp',
+      keyCode: 'Esc'
+    });
+  });
+});
+
+app.on('browser-window-created', async function(e, window) {
+  window.setBackgroundColor('#1A1D1F');
+  window.setMenu(null);
+
+  window.webContents.setUserAgent(userAgent);
+
+  window.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    BrowserWindow.getAllWindows()[0].loadURL(url);
+  });
+
+  window.on('page-title-updated', async function(e, title) {
+    DiscordRPC(title);
+  });
+});
+
+app.on('will-quit', async () => {
+  globalShortcut.unregisterAll();
+});
+
+app.on('window-all-closed', async function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+//< Start main process
